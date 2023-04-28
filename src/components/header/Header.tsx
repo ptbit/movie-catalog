9a8c6d2323e0ce6a54fef8e154ce8bc5ece82366
foxstudent103376 <yashopua@gmail.com>
2023-04-28 18:30:39 +0300

    header menu burger

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { AiOutlineSearch, AiOutlineMenu } from "react-icons/ai";
 import Logo from "../../assets/logo.svg";
-import { AiOutlineSearch } from "react-icons/ai";
 import "./style.css";
 
 export const Header = () => {
+  const [burgerMenuHide, setBurgerMenuHide] = useState(false);
+
+  const handleBurgerMenu = () => {
+    setBurgerMenuHide(!burgerMenuHide);
+  };
+  
   return (
-    <header>
+    <header className={burgerMenuHide ? "header-mobile" : ""}>
       <div className="content-wrapper">
         <NavLink className="logo-container" to="/">
           <img src={Logo} alt="logo" />
         </NavLink>
-        <div className="menu-container">
+
+        <div className={burgerMenuHide ? "menu-container desktop" : "menu-container desktop hide"}>
           <NavLink className="menu-item" to="/movies">
             Movies
           </NavLink>
@@ -19,6 +27,15 @@ export const Header = () => {
           </NavLink>
           <NavLink className="menu-item" to="/">
             <AiOutlineSearch size={"20px"} />
+          </NavLink>
+        </div>
+
+        <div className="menu-container mobile">
+          <NavLink className="menu-item" to="/">
+            <AiOutlineSearch size={"20px"} />
+          </NavLink>
+          <NavLink className="menu-item" to="/">
+            <AiOutlineMenu onClick={handleBurgerMenu} size={"20px"} />
           </NavLink>
         </div>
       </div>
