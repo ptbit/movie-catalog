d03663ae2190ba2c50d2e893f30f59972bdb7ad1
foxstudent103376 <yashopua@gmail.com>
2023-05-23 22:10:38 +0300

    top cast no avatar + styles

---
+++ src/pages/Movie/ActorItem.tsx
@@ -0,0 +1,26 @@
+import noAvatar from "../../assets/no-avatar.png";
+import styles from "./styles.module.css";
+type ActorItemPropsType = {
+  avatar:string,
+  name:string,
+  role:string,
+}
+
+export const ActorItem = ({ name, avatar, role }: ActorItemPropsType) => {
+  if (avatar === 'https://www.themoviedb.org/t/p/w138_and_h175_facenull') {
+    avatar = noAvatar
+  }
+    
+  return (
+    <div className={styles.actor_item}>
+      <div className={styles.actor_avatar}>
+        <img
+          src={avatar}
+          alt={name}
+        />
+      </div>
+      <div className={styles.actor_name}>{name}</div>
+      <div className={styles.actor_role}>{role}</div>
+    </div>
+  );
+};
--- src/pages/Movie/MoviePage.tsx
+++ src/pages/Movie/MoviePage.tsx
@@ -1,17 +1,19 @@
 import { useParams } from "react-router-dom";
 import styles from "./styles.module.css";
-import castPic from "../../assets/movie_page_banner.jpg";
+
 import { useEffect } from "react";
 
 import { useAppDispatch, useAppSelector } from "../../hooks";
 import { getMovie, getTeam } from "../../store/movieSlice";
 import { clearMoviesList } from "../../store/moviesSlice";
+import { ActorItem } from "./ActorItem";
 
 export const MoviePage = () => {
   const appDispatch = useAppDispatch();
   const movie = useAppSelector((state) => state.movie.movie);
   const director = useAppSelector((state) => state.movie.director);
   const writer = useAppSelector((state) => state.movie.writer);
+  const team = useAppSelector((state) => state.movie.team);
 
   const params = useParams();
 
@@ -79,9 +81,7 @@ export const MoviePage = () => {
           <div className={styles.movie_details_info}>
             <div className={styles.movie_details_info_item}>
               <span className={styles.movie_details_info_item_title}>Director:</span>
-              <span className={styles.movie_details_info_item_content}>
-                {director}
-              </span>
+              <span className={styles.movie_details_info_item_content}>{director}</span>
             </div>
           </div>
 
@@ -93,8 +93,24 @@ export const MoviePage = () => {
           </div>
         </div>
       </div>
+      <div className={styles.movie_details_castSectionShadow}></div>
       <div className={styles.movie_details_castSection}>
-        <img src={castPic} alt="logo" />
+        <h2>Top Cast</h2>
+        <div className={styles.actors_row}>
+          {team.map((el, index) => {
+            return (
+              <ActorItem
+                key={index}
+                avatar={"https://www.themoviedb.org/t/p/w138_and_h175_face" + el.profile_path}
+                name={el.name}
+                role={el.character ? el.character : ""}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <div className={styles.movie_details_similarMovie}>
+        <p>Similar Movies</p>
       </div>
     </div>
   );
